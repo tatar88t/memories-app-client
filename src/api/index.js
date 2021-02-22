eaa23220e128d767db4217df271fd556ed8c2188
tatar88t <tatar_88@bk.ru>
2021-02-22 17:43:37 +0300

    fix backend url

--- src/api/index.js
+++ src/api/index.js
@@ -2,10 +2,10 @@ import axios from 'axios';
 
 // const url = "http://localhost:5000/posts";
 
-const API = axios.create({ baseURL: "http://localhost:5000"})
+const API = axios.create({ baseURL: "https://flashback-m.herokuapp.com"})
 
 // const url = "https://flashback-m.herokuapp.com/posts";
-
+// const url = "http://localhost:5000"
 API.interceptors.request.use((req) => {
     if(localStorage.getItem('profile')) {
         req.headers.Authorization = `Bearer ${JSON.parse(localStorage.getItem('profile')).token}`;
